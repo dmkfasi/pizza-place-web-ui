192bd22625570a62101f2a64a7d45c4aff7c1b4c
Andrei Ivanov <dmkfasi@gmail.com>
2020-04-06 18:43:26 +0300

    Renamed

---
+++ src/app/customer/customer.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { CustomerCardComponent } from './customer-card/customer-card.component';
+import { CustomerListComponent } from './customer-list/customer-list.component';
+import { CustomerNewComponent } from './customer-new/customer-new.component';
+import { CustomersRoutingModule } from './customers-routing.module';
+
+// Material
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+
+@NgModule({
+  declarations: [CustomerListComponent, CustomerCardComponent, CustomerNewComponent],
+  imports: [
+    CommonModule,
+    CustomersRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    MatInputModule
+  ]
+})
+export class CustomersModule { }
