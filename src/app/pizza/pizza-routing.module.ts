44ea97dbcef0d443668a3528b5b634780d5f4447
Andrei Ivanov <dmkfasi@gmail.com>
2020-04-13 12:39:22 +0300

    Shopping cart route added

--- src/app/pizza/pizza-routing.module.ts
+++ src/app/pizza/pizza-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PizzaListComponent } from './pizza-list/pizza-list.component';
+import { PizzaDetailsComponent } from './pizza-details/pizza-details.component';
 
 
 const routes: Routes = [
@@ -8,6 +9,10 @@ const routes: Routes = [
     path: '',
     component: PizzaListComponent
   },
+  {
+    path: ':name',
+    component: PizzaDetailsComponent
+  }
 ];
 
 @NgModule({
