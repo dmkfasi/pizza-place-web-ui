3aef4b973b8e772fa39448a9a4c8a01280559259
Andrei Ivanov <dmkfasi@gmail.com>
2020-04-13 12:39:53 +0300

    Refreshed look with Bootstrap

--- src/app/pizza/pizza-list/pizza-list.component.ts
+++ src/app/pizza/pizza-list/pizza-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PizzaService } from 'src/app/services/pizza-service.service';
 import { Pizza } from 'src/app/interfaces/pizza';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-pizza-list',
@@ -8,14 +9,20 @@ import { Pizza } from 'src/app/interfaces/pizza';
   styleUrls: ['./pizza-list.component.css']
 })
 export class PizzaListComponent implements OnInit {
-  protected pizzas: Pizza[];
+  public pizzas: Pizza[];
 
-  constructor(private service: PizzaService) { }
+  constructor(private pizzaService: PizzaService,
+    private cartService: CartService) { }
 
   ngOnInit(): void {
-    this.service.getPizzaList().subscribe(data => {
+    this.pizzaService.getPizzaList().subscribe(data => {
       this.pizzas = data;
     });
+  }
+
+  addToCart(pizza) {
+    this.cartService.addToCart(pizza);
+    window.alert('Your product has been added to the cart!');
   }
 
 }
