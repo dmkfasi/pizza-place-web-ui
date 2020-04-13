6943d3716b56fb95c45caba0161e60a089a981d7
Andrei Ivanov <dmkfasi@gmail.com>
2020-04-14 00:31:35 +0300

    Event subscription for Cart icon added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
+import { CartService } from './services/cart.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+  shoppingCartIsEmpty: boolean = true;
+
+  constructor(private cartService: CartService) { }
+
+  ngOnInit() {
+    const cartObservable = this.cartService.setupObservable();
+    cartObservable.subscribe(() => {
+      this.shoppingCartIsEmpty = false;
+    });
+  }
 }
