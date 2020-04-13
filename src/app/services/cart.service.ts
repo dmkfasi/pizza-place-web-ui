c4334ce3490a0dc080d6b976970d8ba71aa078ae
Andrei Ivanov <dmkfasi@gmail.com>
2020-04-13 11:12:25 +0300

    Basic functionality

---
+++ src/app/services/cart.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { Pizza } from '../interfaces/pizza';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class CartService {
+    items = [];
+    itemsCount: { id: number, count: number };
+
+    constructor() { }
+
+    addToCart(product) {
+
+        this.itemsCount.id = product.id;
+        this.itemsCount.count += 1;
+
+        this.items.push(product);
+
+        console.log(this.itemsCount);
+    }
+
+    getItems() {
+        return this.items;
+    }
+
+    clearCart() {
+        this.items = [];
+        return this.items;
+    }
+}
