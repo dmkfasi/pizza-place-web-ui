a0dae282c44a18844e397e6ca7a3e24169fafd9b
Andrei Ivanov <dmkfasi@gmail.com>
2020-04-14 02:14:17 +0300

    Delivery addition implemented, Total Cost accumulator implemented (sort of)

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,17 +1,16 @@
-import { Injectable, Output } from '@angular/core';
-import { Pizza } from '../interfaces/pizza';
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Delivery } from '../interfaces/Delivery';
 
 @Injectable({
     providedIn: 'root'
 })
 export class CartService {
-    items = [];
-    itemsCount: { id: number, count: number };
-    observer;
+    private observer;
+    private items = [];
+    private totalCost: number = 0;
 
-    constructor() {
-    }
+    constructor() { }
 
     setupObservable() {
         const cartObservable = new Observable(observer => {
@@ -21,11 +20,28 @@ export class CartService {
         return cartObservable;
     }
 
-    addToCart(product) {
+    addToCart(product: any) {
+        // Stack products onto array
         this.items.push(product);
-        this.observer.next(this.items[-1]);
-        this.observer.complete();
-    }    
+        this.updateTotalCost(product.basePrice);
+
+        this.observer.next(product.name);
+    }
+
+    addDelivery(delivery: Delivery) {
+        // Add as if it were a catalog item
+        this.addToCart(delivery);
+    }
+
+    updateTotalCost(cost: number) {
+        // Update total cost property
+        // FIX ME type casting :(
+        this.totalCost += cost;
+    }
+
+    getTotalCost() {
+        return this.totalCost;
+    }
 
     getItems() {
         return this.items;
@@ -33,6 +49,8 @@ export class CartService {
 
     clearCart() {
         this.items = [];
+        this.totalCost = 0;
+
         return this.items;
     }
 }
