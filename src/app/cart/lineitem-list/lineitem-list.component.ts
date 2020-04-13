6bee61b566c0ab5145d544a5f58363b25be7d2c3
Andrei Ivanov <dmkfasi@gmail.com>
2020-04-13 12:40:15 +0300

    Basic shopping cart functionality

--- src/app/cart/lineitem-list/lineitem-list.component.ts
+++ src/app/cart/lineitem-list/lineitem-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-lineitem-list',
@@ -6,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lineitem-list.component.css']
 })
 export class LineitemListComponent implements OnInit {
+  items = [];
 
-  constructor() { }
+  constructor(private cartService: CartService) { }
 
   ngOnInit(): void {
+    this.items = this.cartService.getItems();
   }
 
 }
