e9373e8c5e2187f301ce41f2d2dca42e7f951189
Andrei Ivanov <dmkfasi@gmail.com>
2020-04-13 12:38:23 +0300

    Getting rid of Material components

--- src/app/pizza/pizza.module.ts
+++ src/app/pizza/pizza.module.ts
@@ -8,14 +8,18 @@ import { PizzaListComponent } from './pizza-list/pizza-list.component';
 // Material
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatStepperModule } from '@angular/material/stepper';
+import { PizzaDetailsComponent } from './pizza-details/pizza-details.component';
 
 @NgModule({
-  declarations: [PizzaListComponent],
+  declarations: [PizzaListComponent, PizzaDetailsComponent],
   imports: [
     CommonModule,
     PizzaRoutingModule,
@@ -24,6 +28,9 @@ import { MatStepperModule } from '@angular/material/stepper';
 
     MatAutocompleteModule,
     MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatChipsModule,
     MatFormFieldModule,
     MatGridListModule,
     MatIconModule,
