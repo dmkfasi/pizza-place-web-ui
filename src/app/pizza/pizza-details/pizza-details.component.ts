56173bdcc40e74318b0274bdeb575bdcf0c8db9e
Andrei Ivanov <dmkfasi@gmail.com>
2020-04-13 11:09:05 +0300

    Basic layout for individual item

---
+++ src/app/pizza/pizza-details/pizza-details.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { CartService } from '../../services/cart.service';
+import { PizzaService } from 'src/app/services/pizza-service.service';
+import { Pizza } from 'src/app/interfaces/pizza';
+
+@Component({
+  selector: 'app-pizza-details',
+  templateUrl: './pizza-details.component.html',
+  styleUrls: ['./pizza-details.component.css']
+})
+
+export class PizzaDetailsComponent implements OnInit {
+  public pizza: Pizza;
+  private pizzaName: string;
+
+  constructor(private route: ActivatedRoute,
+    private cartService: CartService,
+    private pizzaService: PizzaService) { }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      this.pizzaName = params.get('name');
+    });
+
+    this.pizzaService.getPizzaByName(this.pizzaName).subscribe(data => {
+      this.pizza = data;
+    });
+  }
+
+  addToCart(pizza) {
+    this.cartService.addToCart(pizza);
+    window.alert('Your product has been added to the cart!');
+  }
+}
