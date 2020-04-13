271928101193cea1f91356d70fdea63d6165c538
Andrei Ivanov <dmkfasi@gmail.com>
2020-04-14 02:16:24 +0300

    clearCart() and addDelivery() methods introduced

--- src/app/cart/lineitem-list/lineitem-list.component.ts
+++ src/app/cart/lineitem-list/lineitem-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from 'src/app/services/cart.service';
+import { Delivery } from "../../interfaces/Delivery";
 
 @Component({
   selector: 'app-lineitem-list',
@@ -8,11 +9,33 @@ import { CartService } from 'src/app/services/cart.service';
 })
 export class LineitemListComponent implements OnInit {
   items = [];
+  hasDelivery: boolean = false;
+  totalCost: number = 0;
+
+  // TODO: fetch from backend app
+  delivery: Delivery = {
+    name: 'Delivery',
+    basePrice: 3.49,
+    baseCurrency: 'EUR'
+  };
 
   constructor(private cartService: CartService) { }
 
   ngOnInit(): void {
     this.items = this.cartService.getItems();
+    this.totalCost = this.cartService.getTotalCost();
   }
 
+  addDelivery() {
+    this.cartService.addDelivery(this.delivery);
+    this.hasDelivery = true;
+  }
+
+  clearCart() {
+    this.cartService.clearCart();
+    this.hasDelivery = false;
+
+    // TODO: replace me with Router Events Subscription
+    window.location.reload();
+  }
 }
