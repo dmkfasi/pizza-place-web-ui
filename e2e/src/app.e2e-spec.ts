7107263d12becc0097f3481977e5c0fd4cd0e8f0
Andrei Ivanov <dmkfasi@gmail.com>
2020-04-15 15:07:30 +0300

    Basic test fixed and passed ;)

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('OrderManagerWebUI app is running!');
+    expect(page.getTitleText()).toEqual('The best Pizza in Town!');
   });
 
   afterEach(async () => {
