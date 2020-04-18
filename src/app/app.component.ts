c8aef2b9cc963bbe28b366306548dff81aa3d213
Andrei Ivanov <dmkfasi@gmail.com>
2020-04-18 11:22:00 +0300

    Items quantity notification display within the Cart badge added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CartService } from './services/cart.service';
+import { Subscription } from 'rxjs';
 declare var $: any;
 
 @Component({
@@ -9,23 +10,28 @@ declare var $: any;
 })
 
 export class AppComponent {
-  // cartSubscription: Subscription;
-  shoppingCartIsEmpty: boolean = true;
+  cartSubscription: Subscription;
+
   addedItemName: string = 'Hurry up some buy!';
+  shoppingCartIsEmpty: boolean = true;
+  shoppingCartItemsQuantity: number = 0;
 
   constructor(private cartService: CartService) { }
 
   ngOnInit() {
     // Display notification upon Shopping Cart icon and update Tooltip for it
-    this.cartService.getUpdates().subscribe((itemName: string) => {
-      this.shoppingCartIsEmpty = false;
+    this.cartSubscription = this.cartService.getUpdates().subscribe((itemName: string) => {
       this.addedItemName = itemName;
+      this.shoppingCartItemsQuantity = this.cartService.getLineitemQuantity();
+
+      this.shoppingCartIsEmpty = (this.shoppingCartItemsQuantity == 0)
+        ? true
+        : false;
     });
   }
 
   ngOnDestroy(): void {
-    // FIXME
-    // this.cartService.getUpdates().unsubscribe();
+    this.cartSubscription.unsubscribe();
   }
 
   static displayToast(toastName: string): void {
